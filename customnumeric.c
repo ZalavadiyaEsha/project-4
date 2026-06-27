f7f7f3020cea1153de8f282cff6ae0da318ec6cb
ZalavadiyaEsha <zalavadiyaesha@gmail.com>
2026-06-27 11:12:23 +0530

    Add files via upload

---
+++ customnumeric.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int space;
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        space = (5 - i) * 2;
+        for(int k = 1; k <= space; k++)
+        {
+            printf("  ");
+        }
+        for(int j = i; j >= 1; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ pyramid.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+    int n = 5;
+    for (int i = n; i >= 1; i--) 
+    {
+        for (int j = 1; j < i; j++) 
+        {
+            printf("  ");
+        }
+        for (int j = i; j <= n; j++) 
+        {
+            printf("%d ", j);
+        }
+        for (int k = n - 1; k >= i; k--) 
+        {
+            printf("%d ", k);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
